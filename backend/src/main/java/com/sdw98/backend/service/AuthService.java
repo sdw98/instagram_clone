4934e75f1bb997412fcc461804bf8852e7cd51c5
sdw98 <sdw9851@gmail.com>
2025-09-17 13:08:53 +0900

    Auth ing...

--- backend/src/main/java/com/sdw98/backend/service/AuthService.java
+++ backend/src/main/java/com/sdw98/backend/service/AuthService.java
@@ -1,7 +1,15 @@
 package com.sdw98.backend.service;
 
+import com.sdw98.backend.dto.AuthResponse;
+import com.sdw98.backend.dto.RegisterRequest;
+import com.sdw98.backend.dto.UserDto;
+import com.sdw98.backend.entity.AuthProvider;
+import com.sdw98.backend.entity.User;
+import com.sdw98.backend.exception.UserAlreadyExistsException;
 import com.sdw98.backend.repository.UserRepository;
+import com.sdw98.backend.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +19,35 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        if(userRepository.existsByUsername(request.getUsername())) {
+            throw new UserAlreadyExistsException("Username already exists");
+        }
+
+        if(userRepository.existsByEmail(request.getEmail())) {
+            throw new UserAlreadyExistsException("Email already exists");
+        }
+
+        User user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .fullName(request.getFullName())
+                .provider(AuthProvider.LOCAL)
+                .build();
+
+        user = userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        return AuthResponse.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .user(UserDto.fromEntity(user))
+                .build();
+    }
 }
